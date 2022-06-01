26a47364c73c3733cad4753bb005a471da080855
SALIMAHya <103593349+SALIMAHya@users.noreply.github.com>
2022-06-01 20:20:41 +0800

    Add files via upload

---
+++ precache-manifest.1bdec53d26b11a3afe6e4c7f2970d37b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ada81f47d16ce38d1a",
+    "url": "game.37bb88d3078809c0b57a.js"
+  },
+  {
+    "revision": "cfaefed83226aceb68e8",
+    "url": "vendors.feee7ddb057a807f8dd0.js"
+  },
+  {
+    "revision": "9c19efece75068ba7e1b3b821bea8533",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "e13c18e6eb09749b127b84bfefc1a209",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "e55a8c3c7e9d2943a33feeb484b4e1bf",
+    "url": "assets/backgrounds/cloud_lonely.png"
+  },
+  {
+    "revision": "8483c2c3896ff8db5048261fc66e169b",
+    "url": "assets/backgrounds/clouds_BG.png"
+  },
+  {
+    "revision": "89459e19dfccb74f48899f878f252408",
+    "url": "assets/backgrounds/clouds_MG_1_lightened.png"
+  },
+  {
+    "revision": "6720e51f519cf129903d74f7869b35a0",
+    "url": "assets/backgrounds/clouds_MG_2.png"
+  },
+  {
+    "revision": "59a4e4378c407f1521e4f131cf8236bc",
+    "url": "assets/backgrounds/clouds_MG_1.png"
+  },
+  {
+    "revision": "b70e82be1998cd7156bfc5ac580fb0bf",
+    "url": "assets/backgrounds/clouds_MG_3.png"
+  },
+  {
+    "revision": "beb79e0fba09d06d6f8c764c8e447233",
+    "url": "assets/backgrounds/mountains_lightened.png"
+  },
+  {
+    "revision": "78112502adf5443224059311cdef86ba",
+    "url": "assets/backgrounds/mountains.png"
+  },
+  {
+    "revision": "949e15e96f13a4a0945d05982690a1e2",
+    "url": "assets/fonts/arcade.ttf"
+  },
+  {
+    "revision": "fbb5a642c01d22443d43ef6f62fac9a2",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "d7bc86b430abbee54a71bf49b7e7bdce",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "4b33b18f79aab29e30314404e9124458",
+    "url": "assets/images/dialog.png"
+  },
+  {
+    "revision": "1db0dd741c9a8672080fbd920f4bdc1e",
+    "url": "assets/images/button.png"
+  },
+  {
+    "revision": "47a22af0104ee3c17678f8d9cb249011",
+    "url": "assets/sprites/door.png"
+  },
+  {
+    "revision": "2123a11c0fc4e4aad3f892a96a09eae6",
+    "url": "assets/sprites/empty.png"
+  },
+  {
+    "revision": "554ab99d699ac203e2b21ae4d9948a66",
+    "url": "assets/sprites/love_chest.png"
+  },
+  {
+    "revision": "d684da2114fe3f1df12d1bbd63753f42",
+    "url": "assets/sprites/heart.png"
+  },
+  {
+    "revision": "39dee448de074a97f6d49cac82cba6a6",
+    "url": "assets/tilemaps/atlas.tsx"
+  },
+  {
+    "revision": "f35199e325ded5b220bb8514f5e32f18",
+    "url": "assets/sprites/kk.png"
+  },
+  {
+    "revision": "656c95aa0cf6db7469272cfd87111d62",
+    "url": "assets/tilemaps/base.tmx"
+  },
+  {
+    "revision": "cad5a792c2b6e39f4eae83a31c9e6e56",
+    "url": "assets/tilemaps/atlas.png"
+  },
+  {
+    "revision": "7dff1540d2af72a0002e55db059a77e3",
+    "url": "assets/tilemaps/extruded.png"
+  },
+  {
+    "revision": "1de27531f32f81fadb42b4bdfaee8af9",
+    "url": "assets/tilemaps/base.json"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.1bdec53d26b11a3afe6e4c7f2970d37b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest);
+
